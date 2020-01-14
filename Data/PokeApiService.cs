d152da6555f7ad066113c6659f3803a25adc5538
Julian Cook <jcook@smcpartners.com>
2020-01-14 13:16:10 -0500

    Feat: Set up pkmnData service an ui to get some info about pkmn.

---
+++ Data/PokeApiService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using PokeApiNet;
+using PokeApiNet.Models;
+
+namespace TestBlazorApp.Data
+{
+    public class PokeApiService
+    {
+        public async Task<PokeData> GetPokemonData(string pkmnName)
+        {
+            PokeApiClient _pokeApiClient = new PokeApiClient();
+
+            Pokemon pkmn = await _pokeApiClient.GetResourceAsync<Pokemon>(pkmnName);
+
+            PokeData pokeData = new PokeData
+            {
+                DexEntry = pkmn.Id,
+                Name = pkmn.Name,
+                PkmnSprite = pkmn.Sprites.FrontDefault,
+                PkmnShinySprite = pkmn.Sprites.FrontShiny
+            };
+
+            return pokeData;
+        }
+    }
+}
---
+++ Data/PokeData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestBlazorApp.Data
+{
+    public class PokeData
+    {
+        public string Name { get; set; }
+        public int DexEntry { get; set; }
+        public string PkmnSprite {get; set;}
+        public string PkmnShinySprite {get; set;}
+    }
+}
